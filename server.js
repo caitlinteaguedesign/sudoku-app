9285a49f13dd136ab05175d3265d0b1e6f165132
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-04-07 09:36:36 -0500

    add env for protocol

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const http = require('http');
+// setup
 const app = require('./app');
 
+// constants
 const PORT = process.env.PORT || 4000;
-const server = http.createServer(app);
+
+// declare protocol - might use https when on digital ocean
+let protocol;
+
+if(process.env.NODE_ENV !== "production") {
+   protocol = require('http');
+}
+else {
+   protocol = require('https');
+}
+
+// setup server
+server = protocol.createServer(app);
 
 // Listen for connections, changes
 server.listen(PORT, console.log(`Server is starting at ${PORT}`));
