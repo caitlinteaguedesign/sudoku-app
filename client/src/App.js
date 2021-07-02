d6052f2e42756d2e740050848f6399f5a3bca5f0
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-07-02 18:16:33 -0500

    client cleanup, start

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+import logo from './img/logo.svg';
+import './styles/main.scss';
+
+import Home from './views/Home';
+import Browse from './views/Browse';
+import Create from './views/Create';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header className="header">
+        <Link to="/" className="header__logo">
+          <img src={logo} alt="logo" width="36" height="36" />
+          Sudoku
+        </Link>
+        <Link to="/browse">Browse</Link>
+        <Link to="/create">Create</Link>
       </header>
-    </div>
+
+      <main className="main">
+        <Switch>
+          <Route path="/browse">
+            <Browse />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
