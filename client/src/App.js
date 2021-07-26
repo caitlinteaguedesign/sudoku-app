fa4b5b75e448e19a68bd1b94f1d5b3a9fe2f3eef
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-07-26 15:43:11 -0500

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import PrivateRoute from './util/PrivateRoute';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import logo from './img/logo.svg';
 import './styles/main.scss';
@@ -13,55 +15,57 @@ import Create from './views/Create';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
+    <Provider store={store}>
+      <Router>
+        <div className="app">
 
-        <Switch>
-          
-          <Route path="/register">
-            <Register />
-          </Route>
+          <Switch>
+            
+            <Route path="/register">
+              <Register />
+            </Route>
 
-          <Route path="/login">
-            <Login />
-          </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-          <PrivateRoute path="/">
-          
-            <header className="header">
+            <PrivateRoute path="/">
+            
+              <header className="header">
 
-              <div className="header__container">
-                <Link to="/" className="header__logo">
-                  <img src={logo} alt="logo" width="36" height="36" />
-                  Sudoku
-                </Link>
-                <Link to="/browse" className="link link_style-text">Browse</Link>
-                <Link to="/create" className="link link_style-text">Create</Link>
-              </div>
+                <div className="header__container">
+                  <Link to="/" className="header__logo">
+                    <img src={logo} alt="logo" width="36" height="36" />
+                    Sudoku
+                  </Link>
+                  <Link to="/browse" className="link link_style-text">Browse</Link>
+                  <Link to="/create" className="link link_style-text">Create</Link>
+                </div>
 
-              <UserHandle />
+                <UserHandle />
 
-            </header>
+              </header>
 
-            <main className="main">
-              <Switch>
-                <Route path="/browse">
-                  <Browse />
-                </Route>
-                <Route path="/create">
-                  <Create />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </main>
-          </PrivateRoute>
+              <main className="main">
+                <Switch>
+                  <Route path="/browse">
+                    <Browse />
+                  </Route>
+                  <Route path="/create">
+                    <Create />
+                  </Route>
+                  <Route path="/">
+                    <Home />
+                  </Route>
+                </Switch>
+              </main>
+            </PrivateRoute>
 
-        </Switch>
+          </Switch>
 
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
