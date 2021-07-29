6e53af1a2a16fe46b3487efb44f4623ee1c20572
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-07-29 16:13:41 -0500

    SAVE

--- client/src/game/Board.js
+++ client/src/game/Board.js
@@ -17,9 +17,11 @@ function Row(row, i) {
 function Cell(cell, i) {
    // TO DO: pass color/style classes down by cell level
 
+   // TO DO: USE START GRID TO DETERMINE READ ONLY CELLS
    if(cell !== 0) {
       return <div key={`cell_${i}`} className="board__cell board__cell--readonly">{cell}</div>
    }
+   // TO DO: USE USER STORED VALUE (BLANK IF 0)
    else {
       // TO DO: replace non 1-9 with empty spaces
       return <input key={`cell_${i}`} type="text" pattern="[1-9]" maxLength="1" className="board__cell" />
