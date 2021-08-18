38ddf27b8a30a62c1384511c73bd6891f0c19869
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-08-18 09:19:53 -0500

    save

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,6 +3,7 @@ export default function Home() {
       <main className="main main_theme-homepage">
          <div className="page">
             {/* home page soon */}
+            * homepage coming soon *
          </div>
       </main>
    )
