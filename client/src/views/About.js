e2b3d75fbf740fcea991a302de20a35157eff887
Caitlin Teague Doerr <design@caitlinteague.com>
2023-04-18 18:45:40 -0500

    save changes

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -2,8 +2,6 @@ export default function About() {
    return (
       <main className="main">
          <div className="page">
-            {/* about page soon */}
-            * about coming soon *
          </div>
       </main>
    )
