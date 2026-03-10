ebbef5d62658cd503dcb25d8eb16717e6eeaa5d7
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-09 22:49:25 -0500

    refactor createStore for legacy & dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,21 +1,25 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const devTools = 
-   process.env.NODE_ENV === 'production'
-      ? applyMiddleware(...middleware)
-      : compose( applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const devTools =
+  process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )
+    : applyMiddleware(...middleware);
 
-const store = createStore(
-   rootReducer,
-   initialState,
-   devTools
-)
+const store = createStore(rootReducer, initialState, devTools);
 
 export default store;
-
