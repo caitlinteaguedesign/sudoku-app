c5ecd14ab0a4f7ca1322a797c2e4218856839a6d
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-13 19:51:15 -0500

    correct function name

--- api/middleware/login.js
+++ api/middleware/login.js
@@ -1,25 +1,24 @@
-const Validator = require('validator');
-const isEmpty = require('is-empty');
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 
-module.exports = function validateRegisterInput (data) {
-   let errors = {};
+module.exports = function validateLoginInput(data) {
+  let errors = {};
 
-   data.email = !isEmpty(data.email) ? data.email : '';
-   data.password = !isEmpty(data.password) ? data.password : '';
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
 
-   if(Validator.isEmpty(data.email)) {
-      errors.login_email = 'An email is required';
-   }
-   else if (!Validator.isEmail(data.email)) {
-      errors.login_email = 'Email is invalid';
-   }
+  if (Validator.isEmpty(data.email)) {
+    errors.login_email = "An email is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.login_email = "Email is invalid";
+  }
 
-   if(Validator.isEmpty(data.password)) {
-      errors.login_password = 'A password is required';
-   }
+  if (Validator.isEmpty(data.password)) {
+    errors.login_password = "A password is required";
+  }
 
-   return {
-      errors,
-      isValid: isEmpty(errors)
-   }
-}
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
