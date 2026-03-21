f18b567e2a418e845a82dcc5d1fa84c43f4e6241
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-21 11:11:25 -0500

    update is-empty

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,34 +1,33 @@
-import { SET_CURRENT_USER, CAN_LOGOUT, USER_LOADING } from '../actions/types';
-
-const isEmpty = require('is-empty');
+import { SET_CURRENT_USER, CAN_LOGOUT, USER_LOADING } from "../actions/types";
+import isEmpty from "is-empty";
 
 const initialState = {
-   isAuthenticated: false,
-   user: {},
-   loading: false,
-   canLogout: true
-}
+  isAuthenticated: false,
+  user: {},
+  loading: false,
+  canLogout: true,
+};
 
 export default function authReducer(state = initialState, action) {
-   switch (action.type) {
-      case SET_CURRENT_USER:
-         return {
-            ...state,
-            isAuthenticated: !isEmpty(action.payload),
-            user: action.payload,
-            canLogout: true
-         }
-      case CAN_LOGOUT:
-         return {
-            ...state,
-            canLogout: action.payload
-         }
-      case USER_LOADING:
-         return {
-            ...state,
-            loading: true
-         }
-      default:
-         return state;
-   }
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload,
+        canLogout: true,
+      };
+    case CAN_LOGOUT:
+      return {
+        ...state,
+        canLogout: action.payload,
+      };
+    case USER_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
 }
