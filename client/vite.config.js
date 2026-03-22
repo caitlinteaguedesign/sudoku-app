4d33a2c3ffe824288116d8cede89fd46e9c99ed7
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-22 18:04:19 -0500

    silence dart scss warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api"],
+      },
+    },
+  },
   plugins: [
     react(),
     svgrPlugin({
