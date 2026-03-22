24b58a254c9920331823d4acaa1bcdbb1a537d8a
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-22 17:45:07 -0500

    trigger browser to re-render after logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,110 +1,107 @@
-import axios from 'axios';
-import setAuthToken from '../util/setAuthToken';
-import jwt_decode from 'jwt-decode';
+import axios from "axios";
+import setAuthToken from "../util/setAuthToken";
+import jwt_decode from "jwt-decode";
 
 import {
-   GET_ERRORS,
-   SET_CURRENT_USER,
-   USER_LOADING,
-   CAN_LOGOUT
-} from './types';
+  GET_ERRORS,
+  SET_CURRENT_USER,
+  USER_LOADING,
+  CAN_LOGOUT,
+} from "./types";
 
 // Register User
-export const registerUser = (userData, history) => dispatch => {
-   axios
-      .post('/users/register', userData)
-      .then(
-         res => {
-            // reset errors
-            dispatch(resetErrors());
+export const registerUser = (userData, history) => (dispatch) => {
+  axios
+    .post("/users/register", userData)
+    .then((res) => {
+      // reset errors
+      dispatch(resetErrors());
 
-            // redirect to login after successful register
-            history.push('/login');
-         }
-      ) 
-      .catch(err => 
-         dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-         })
-      );
-}
+      // redirect to login after successful register
+      history.push("/login");
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
 
 // Login
-export const loginUser = userData => dispatch => {
-   axios
-      .post('/users/login', userData)
-      .then(res => {
-         // save to local storage
-         const { token } = res.data;
-         localStorage.setItem('jwtToken', token);
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/users/login", userData)
+    .then((res) => {
+      // save to local storage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
 
-         // set token to auth header
-         setAuthToken(token);
+      // set token to auth header
+      setAuthToken(token);
 
-         // decode to get user data
-         const decoded = jwt_decode(token);
+      // decode to get user data
+      const decoded = jwt_decode(token);
 
-         // set current user
-         dispatch(setCurrentUser(decoded));
+      // set current user
+      dispatch(setCurrentUser(decoded));
 
-         // reset errors
-         dispatch(resetErrors());
-      })
-      .catch( err => 
-         dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-         })
-      );
-}
+      // reset errors
+      dispatch(resetErrors());
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
 
 // Set logged in user
-export const setCurrentUser = decoded => {
-   return {
-      type: SET_CURRENT_USER,
-      payload: decoded
-   }
-}
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
+};
 
 // Set if the user can log out
-export const setCanLogout = decoded => {
-   return {
-      type: CAN_LOGOUT,
-      payload: decoded
-   }
-}
+export const setCanLogout = (decoded) => {
+  return {
+    type: CAN_LOGOUT,
+    payload: decoded,
+  };
+};
 
 // User loading
 export const setUserLoading = () => {
-   return {
-      type: USER_LOADING
-   }
-}
+  return {
+    type: USER_LOADING,
+  };
+};
 
-// Reset Errors 
+// Reset Errors
 export const resetErrors = () => {
-   return {
-      type: GET_ERRORS,
-      payload: {}
-   }
-}
+  return {
+    type: GET_ERRORS,
+    payload: {},
+  };
+};
 
 // Log user out
-export const logoutUser = (history) => dispatch => {
+export const logoutUser = (history) => (dispatch) => {
+  // Remove token from local storage
+  localStorage.removeItem("jwtToken");
 
-   // Remove token from local storage
-   localStorage.removeItem('jwtToken');
+  // Remove auth header for future requests
+  setAuthToken(false);
 
-   // Remove auth header for future requests
-   setAuthToken(false);
+  // reset can logout
+  dispatch(setCanLogout(true));
 
-   // reset can logout
-   dispatch(setCanLogout(true));
+  // Set current user to empty object {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
 
-   // Set current user to empty object {} which will set isAuthenticated to false
-   dispatch(setCurrentUser({}));
-
-   // redirect to homepage after logout
-   history.push('/?logout');
-}
+  // redirect to homepage after logout
+  history.push("/");
+};
