f38bf5745a97e937327a5fb72299eef16d572a49
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-25 19:56:17 -0500

    update footer, link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,22 @@
 export default function Footer(props) {
-   const { theme } = props;
-   const year = new Date().getFullYear();
+  const { theme } = props;
+  const year = new Date().getFullYear();
 
-   return (
-      <footer className={`footer footer_theme-${theme}`}>
-
-         <p className="small">&copy;{year} Caitlin Teague Doerr 
-            <br/> To contact owner/developer, send an email to <a className="link_style-text" href="mailto:design@caitlinteague.com">design@caitlinteague.com</a> 
-            <br/> <a className="link_style-text" href="https://caitlinteague.com/builds">View more projects in Caitlin's portfolio</a>
-         </p>
-
-      </footer>
-   )
+  return (
+    <footer className={`footer footer_theme-${theme}`}>
+      <p className="small">&copy;{year} Caitlin Teague Doerr</p>
+      <p className="small">
+        To contact owner/developer,{" "}
+        <a className="link_style-text" href="mailto:design@caitlinteague.com">
+          send an email
+        </a>{" "}
+        to <span className="text-bold">design@caitlinteague.com</span>
+      </p>
+      <p className="small">
+        <a className="link_style-text" href="https://caitlinteague.com/builds">
+          View more projects in Caitlin's portfolio
+        </a>
+      </p>
+    </footer>
+  );
 }
